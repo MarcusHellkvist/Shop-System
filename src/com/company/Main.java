f44f597d6fd14f215f4017ea8c80ab77812cd438
Marcus Hellkvist <maco@Marcuss-MacBook-Air.local>
2020-09-25 11:01:22 +0200

    fixed the welcome message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome");
-
         Shop shop = GameSystem.initShop();
         shop.showAmulets();
         shop.showHelms();
