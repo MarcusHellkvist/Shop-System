2f078c58953c0cc4bba2765384b2c817c9eac913
Marcus Hellkvist <maco@Marcuss-MacBook-Air.local>
2020-09-25 10:41:32 +0200

    added a welcome text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome");
+
         Shop shop = GameSystem.initShop();
         shop.showAmulets();
         shop.showHelms();
